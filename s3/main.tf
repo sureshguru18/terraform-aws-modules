0bd935bf5d0943eebdfa1e9d7704c2270148c0a5
root <root@ip-172-31-42-137.ec2.internal>
2024-02-13 03:19:48 +0000

    Updated the module

--- s3/main.tf
+++ s3/main.tf
@@ -2,8 +2,5 @@ resource "aws_s3_bucket" "default" {
   count         = var.enabled ? 1 : 0
   bucket        = var.bucket_name
   force_destroy = var.force_destroy
-
-  object_lock_enabled = var.object_lock_enabled
-
   tags = var.tags
 }
--- s3/variables.tf
+++ s3/variables.tf
@@ -13,15 +13,14 @@ variable "bucket_name" {
   description = "Bucket name. If provided, the bucket will be created with this name instead of generating the name from the context"
 }
 
-variable "object_lock_configuration" {
-  type = object({
-    mode  = string # Valid values are GOVERNANCE and COMPLIANCE.
-    days  = number
-    years = number
-  })
-  default     = null
-  description = "A configuration for S3 object locking. With S3 Object Lock, you can store objects using a `write once, read many` (WORM) model. Object Lock can help prevent objects from being deleted or overwritten for a fixed amount of time or indefinitely."
+variable "enabled" {
+  type        = bool
+  default     = true
+  description = "If provided, S3 bucket will be created." 
+
 }
+
+
 
 variable "force_destroy" {
   type        = bool
